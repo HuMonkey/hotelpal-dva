3cee72847158c33af048e6f8da3b75cf73ab6a93
hujj0908 <huwanqi@shuwen.com>
2018-08-21 20:49:16 +0800

    fed build

--- src/models/common.js
+++ src/models/common.js
@@ -35,8 +35,8 @@ export default {
                     const origin = encodeURIComponent(location.origin);
                     const search = encodeURIComponent(location.search);
                     const hash = encodeURIComponent(location.hash);
-                    // const redirect = `http://hotelpal.cn/test.html?origin=${origin}&search=${search}&hash=${hash}`
-                    const redirect = location.href;
+                    const redirect = `http://hotelpal.cn/test.html?origin=${origin}`
+                    // const redirect = location.href;
                     location.href = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='
                         + config.appId + '&redirect_uri='
                         + encodeURIComponent(redirect)
